5a874806dc2c51c613086658c4d94ba75a39729f
Andrew Patton <andrew@acusti.ca>
2023-07-17 14:35:11 -0700

    Add and use getEventHandlers helper

--- src/ReactHigherEventProvider.js
+++ src/ReactHigherEventProvider.js
@@ -26,6 +26,15 @@ const ReactHigherEventProvider: React.ComponentType<Props> = forwardRef(
 
         const [eventProps, setEventProps] = useState<EventProps>(({}: any))
 
+        const getEventHandlers = useCallback((eventName: string) => {
+            let eventHandlers = eventsRef.current.get(eventName)
+            if (!eventHandlers) {
+                eventHandlers = new Set()
+                eventsRef.current.set(eventName, eventHandlers)
+            }
+            return eventHandlers
+        }, [])
+
         const dispatchEvent = useCallback(
             (eventName: string, event: SyntheticEvent<>) => {
                 const events: Events = eventsRef.current
@@ -42,14 +51,7 @@ const ReactHigherEventProvider: React.ComponentType<Props> = forwardRef(
 
         const subscribe = useCallback<Subscribe>(
             (eventName: string, handler: EventHandler) => {
-                const events: Events = eventsRef.current
-                let eventHandlers = events.get(eventName)
-
-                if (!eventHandlers) {
-                    eventHandlers = new Set()
-                    events.set(eventName, eventHandlers)
-                }
-
+                const eventHandlers = getEventHandlers(eventName)
                 eventHandlers.add(handler)
 
                 setEventProps((props: EventProps): EventProps => {
@@ -64,9 +66,7 @@ const ReactHigherEventProvider: React.ComponentType<Props> = forwardRef(
 
                 // Return the cleanup function
                 return () => {
-                    const handlers = events.get(eventName)
-                    if (!handlers) return
-
+                    const handlers = getEventHandlers(eventName)
                     handlers.delete(handler)
                     // If there are no more handlers for this event, remove it from eventProps
                     if (!handlers.size) {
